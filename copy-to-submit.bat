a62cc67638faeb86c7faee431c758f6457180411
gam0022 <gam0022@gmail.com>
2024-10-06 22:17:16 +0900

    copy-to-submit.bat修正

--- copy-to-submit.bat
+++ copy-to-submit.bat
@@ -1,10 +1,14 @@
 set srcDir=D:\Dropbox\RTCamp\redflash_rtcamp10\build\
+set srcDataDir=D:\Dropbox\RTCamp\redflash_rtcamp10\data\
 set srcCudaDir=D:\Dropbox\RTCamp\redflash_rtcamp10\redflash\
 set dstDir=D:\Dropbox\RTCamp\gam0022_gpu\
 
-cp %srcDir%bin\Release\run.ps1 %dstDir%
 cp %srcDir%bin\Release\redflash.exe %dstDir%
 cp %srcDir%bin\Release\sutil_sdk.dll %dstDir%
+
+cp %srcDataDir%mesh\portal.obj %dstDir%data\mesh\
+cp %srcDataDir%mesh\door_base.obj %dstDir%data\mesh\
+
 cp %srcCudaDir%redflash.h %dstDir%cuda\
 cp %srcCudaDir%redflash.cu %dstDir%cuda\
 cp %srcCudaDir%bsdf_diffuse.cu %dstDir%cuda\
